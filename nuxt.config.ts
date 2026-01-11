7957ad2e50ba54d5555f27bf5df421804303fc70
CHOI DAE GEON <cdg@aplayz.co.kr>
2026-01-12 08:44:34 +0900

    seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,8 +12,48 @@ export default defineNuxtConfig({
   app: {
     head: {
       link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
-      charset: "utf-16",
-      viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1.0",
+      meta: [
+        {
+          name: "naver-site-verification",
+          content: "블라블라",
+        },
+        {
+          name: "description",
+          content: "진보된 서비스로 AI음악 큐레이션을 리딩하는 뮤직테크 스타트업",
+        },
+        {
+          name: "og:type",
+          content: "website",
+        },
+        {
+          name: "og:title",
+          content: "(주)어플레이즈",
+        },
+        {
+          name: "og:description",
+          content: "진보된 서비스로 AI음악 큐레이션을 리딩하는 뮤직테크 스타트업",
+        },
+        {
+          name: "og:image",
+          content: "https://aplayz.co.kr/media/images/logo_500.png",
+        },
+        {
+          name: "og:url",
+          content: "https://corp.aplayz.co.kr",
+        },
+        {
+          name: "google-site-verification",
+          content: "블라블라",
+        },
+      ],
+      title: "(주)어플레이즈",
+      script: [
+        {
+          src: "//dapi.kakao.com/v2/maps/sdk.js?appkey=4a44a406f85428e304ae880454edaced&libraries=services",
+        },
+      ],
     },
   },
 });
